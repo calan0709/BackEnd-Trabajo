5901ca969de6084c60728ac05e93927457fca595
calan0709 <cardozo9222@gmail.com>
2023-07-03 23:19:53 -0300

    local8080

--- src/app.js
+++ src/app.js
@@ -3,18 +3,16 @@ import productRouter from './routes/product.router.js';
 import cartsRouter from './routes/carts.router.js';
 import __dirname from "./utils.js";
 
-const app = express(); //iniciamos express con la variable APP, la cual contendra todas las funcionalidades de la dependencia.
-//app recibe la peticion, va a reconocer la peticion de tipo usuario y va a redirijirla al archivo product.router.js
-//para que detecte los objetos que envio del codigo le digo que debe soportar el formato epress y json
+const app = express(); 
 app.use(express.json());
-app.use(express.urlencoded({extended: true})); //objetos codificados desde la URL
+app.use(express.urlencoded({extended: true})); 
 app.use(express.static((`${__dirname}/public`)))
 
 /* app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'handlebars'); */
 
-app.use('/api/product', productRouter)// con esta linea de codigo ya me puedo conectar a los miniaplicativos
+app.use('/api/product', productRouter)
 app.use('/api/carts', cartsRouter)
 
-app.listen(8080, ()=> console.log("listening 8080")); //y esa aplicacion de espress esta escuchando en un puerto
+app.listen(8080, ()=> console.log("listening 8080")); 
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,33 +1,26 @@
-import { Router } from "express"; //lo importamos con llaves porque no es el esport default, 
-//el export default es el modulo principal y este es un derivado internopero que pertenece a la libreria estoy importando un submodulo de rutas
+import { Router } from "express";
 
-const router = Router(); //es el miniaplicativo para que yo pueda redirigir
+const router = Router(); 
 
-const products = []; //arreglo vacio para incertar productos
-//GET
-//leemos la informacion de los productos
-router.get('/', (req, res)=>{ //request es la peticion del cliente, response es la repuesta del servidor
-    res.send(products) // solicitamos obtener los productos
+const products = [];
+
+router.get('/', (req, res)=>{ 
+    res.send(products) 
 })
 
-//create/Post
-//crea un producto para insertarlo en nuestro array
 router.post('/', (req, res)=>{
     const product = req.body;
     products.push(product);
     res.send({status: "succes", message: "pet create"})
 })
 
-//UPDATE
-//antualizar el producto, con lo cual ahy que buscarlo por medio de un identificador.
 router.put('/', (req,res)=>{
 
 })
 
-//delete
 
 router.delete('/', (req, res)=>{
     
 })
 
-export default router;//esport por default (aca importante: cuando yo hago un export, lo hago para que otras partes de mi proyecto puedan importarlo)
+export default router;
