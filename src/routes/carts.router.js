33af52bd89bf9b454befad7d28bfafaec7df92da
calan0709 <cardozo9222@gmail.com>
2023-07-08 21:21:40 -0300

    multer

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,27 +1,34 @@
-import { Router } from "express"; 
+import { Router } from "express"; //lo importamos con llaves porque no es el esport default, 
+//el export default es el modulo principal y este es un derivado internopero que pertenece a la libreria estoy importando un submodulo de rutas
 import uploader from "../services/uploader.js";
 
-const router = Router();
+const router = Router(); //es el miniaplicativo para que yo pueda redirigir
 
-const carts = [];
-
+const carts = []; //arreglo vacio para incertar productos
+//GET
+//leemos la informacion de los productos
 router.get('/', (req, res)=>{
     res.send(carts)
 })
 
+//create/Post
+//crea un producto para insertarlo en nuestro array
 router.post('/', uploader.single("image"), (req, res)=>{
     const cart = req.body;
     carts.push(cart);
     res.send ({status: "sucess", messege: "pet create"})
 })
 
+//UPDATE
+//antualizar el producto, con lo cual ahy que buscarlo por medio de un identificador.
 router.put('/', (req,res)=>{
 
 })
 
+//delete
 
 router.delete('/', (req, res)=>{
     
 })
 
-export default router;
+export default router;//esport por default (aca importante: cuando yo hago un export, lo hago para que otras partes de mi proyecto puedan importarlo)
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,26 +1,32 @@
 import { Router } from "express";
 
-const router = Router(); 
+const router = Router(); //es el miniaplicativo para que yo pueda redirigir
 
-const products = [];
-
-router.get('/', (req, res)=>{ 
-    res.send(products) 
+const products = []; //arreglo vacio para incertar productos
+//GET
+//leemos la informacion de los productos
+router.get('/', (req, res)=>{ //request es la peticion del cliente, response es la repuesta del servidor
+    res.send(products) // solicitamos obtener los productos
 })
 
+//create/Post
+//crea un producto para insertarlo en nuestro array
 router.post('/', (req, res)=>{
     const product = req.body;
     products.push(product);
     res.send({status: "succes", message: "pet create"})
 })
 
+//UPDATE
+//antualizar el producto, con lo cual ahy que buscarlo por medio de un identificador.
 router.put('/', (req,res)=>{
 
 })
 
+//delete
 
 router.delete('/', (req, res)=>{
     
 })
 
-export default router;
+export default router;//esport por default (aca importante: cuando yo hago un export, lo hago para que otras partes de mi proyecto puedan importarlo)
