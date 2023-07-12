8774809b87698c11d3327e450ff29335a92d90a9
calan0709 <cardozo9222@gmail.com>
2023-07-12 20:33:11 -0300

    css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express  from "express";
-import { Server } from "socket.io";
-import productRouter from './routes/product.router.js';
-import cartsRouter from './routes/carts.router.js';
+//import { Server } from "socket.io";
+//import productRouter from './routes/product.router.js';
+//import cartsRouter from './routes/carts.router.js';
 import __dirname from "./utils.js";
+import viewsRouter from './routes/views.router.js';
+import handlebars from 'express-handlebars';
 
 const app = express(); //iniciamos express con la variable APP, la cual contendra todas las funcionalidades de la dependencia.
 //app recibe la peticion, va a reconocer la peticion de tipo usuario y va a redirijirla al archivo product.router.js
@@ -10,12 +12,15 @@ const app = express(); //iniciamos express con la variable APP, la cual contendr
 //para que detecte los objetos que envio del codigo le digo que debe soportar el formato epress y json
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); //objetos codificados desde la URL
-app.use(express.static((`${__dirname}/public`)))
+app.use(express.static((`${__dirname}/public`)));
 
-/* app.engine('handlebars', handlebars.engine());
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'handlebars'); */
+app.engine('handlebars', handlebars.engine());//con que motor de plantilla estoy trabajando
+app.set('views', `${__dirname}/views`);// en que directorio estoy trabajando "VIEWS"
+app.set('view engine', 'handlebars');//estamos trabajando con "handlebars"
 
-app.use('/api/product', productRouter)// con esta linea de codigo ya me puedo conectar a los miniaplicativos
-app.use('/api/carts', cartsRouter) 
+
+//app.use('/api/product', productRouter);// con esta linea de codigo ya me puedo conectar a los miniaplicativos
+//app.use('/api/carts', cartsRouter);
+app.use('/', viewsRouter);
+
 app.listen(8080, ()=> console.log("listening 8080")); //y esa aplicacion de espress esta escuchando en un puerto
---
+++ src/routes/views.router.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+
+/* app.get ('/', (req,res)=>{
+    const user = {
+        name : 'Santiago del moro'
+    };
+    res.render('index',user)//renderiza en el archivo INDEX el dato que esta en USER
+}) */
+const food = [ //crea objeto con nombre food
+    {name:"pizza", price:1000},
+    {name:"hambuergesa", price:500},
+    {name:"papas fritas", price:400},
+    {name:"pollo frito", price:600},
+    {name:"pescadofrito", price:900}
+]
+router.get('/', (req, res)=>{
+    const user = {
+        name:'gonza',
+        role:'admin'
+    }
+    res.render('index',{//va a renderizar en INDEX lo que dice a continuacion
+        user,
+        isAdmin:user.role === 'admin',// si en isAdmin, user.role es igual a admin me vas a renderizar food.
+        food
+    })
+})
+
+export default router;
